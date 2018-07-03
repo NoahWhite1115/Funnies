78e3da6722b9cbe982c3d7ea0652fcc7bc04e2b4
N135 <nawhite@ucdavis.edu>
2018-07-02 22:56:12 -0700

    Began GUI for Funnies main library.

--- Funnies.py
+++ Funnies.py
@@ -1,32 +1,37 @@
-#comic class:
+#Funnies 0.00
+#Written by Noah White
+from Tkinter import *
+import ComicLib
 
-#read /w no args gets the most recent comic, loads it under ./comics/name.png/ or ./comics/name.jpg
-#read /w args gets the comic at that index
-#read's args determined by info string
-
-#show returns the file location of the current stored comic. 
-
-#index returns the index the comic is currently set to
-#max_index gets the highest possible index
-
-#name gets the comics name
-
-#read_next() gets the next comic in order (and then calls read)
-#read_prev() gets the prev comic in order (and then calls read)
-
-
+class ComicGUI(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.parent = master
+    
+    def widgets(self):
+        self.parent.title("Funnies: Python Webcomic Browser")
 
 
 def main():
     #read/split config file as a list of 3-tuples (Name,URL,info_string).
-    config_list = readConfig()
+    config_list = ComicLib.readConfig()
+
 
     #create a list holding all the comic objects
+    comic_list = []
+
     for comic in config_list:
-        comic_list.append(comic_object(comic))
+        x = ComicLib.comic_obj(comic[0],comic[1],comic[2],comic[3])
+        comic_list.append(x)
+        x.read()
 
-    #call the read method on all comic objects
-    for comic in comic_list:
-        comic.read()
+    top = Tk()
+    top.geometry("{0}x{1}". \
+        format(top.winfo_screenwidth(), top.winfo_screenheight()))
+    top.update()
 
-    Comic = launchComicGUI()
+    Comic = ComicGUI(top)
+    top.mainloop()
+
+if __name__ == '__main__':
+    main()
