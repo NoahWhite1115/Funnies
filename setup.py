cd7533a9b51e05d956da09475f1f75ddafc1788e
N135 <nawhite@ucdavis.edu>
2018-07-25 13:43:10 -0700

    Added windows support to setup

--- setup.py
+++ setup.py
@@ -7,9 +7,15 @@ Sets up Funnies for use.
 
 from os.path import abspath
 from os.path import join
+from os import name
 
 path = abspath('Funnies.py')
 
-exe_file = open("./funnies", 'w')
-
-exe_file.write("python " + path)
+#linux/mac case
+if name == 'posix':
+    exe_file = open("./funnies", 'w')
+    exe_file.write("python " + path)
+#windows case
+else:
+    exe_file = open("./funnies.bat",'w')
+    exe_file.write("python " + path)
